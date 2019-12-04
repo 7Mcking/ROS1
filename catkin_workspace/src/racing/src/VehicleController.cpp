f5f67c7610a005d20dbc133fba0f3183ef5eb9cb
Michael Hoß <michael.hoss@rwth-aachen.de>
2019-12-04 20:50:02 +0100

    [MHO] remove target value computations to create exercise

--- catkin_workspace/src/racing/src/VehicleController.cpp
+++ catkin_workspace/src/racing/src/VehicleController.cpp
@@ -20,30 +20,11 @@
 #include <cmath>
 
 void VehicleController::computeTargetVelocity() {
-  const float FACTOR = 2.0f;
-  const float FACTOR2 = 8.0f;
-  const float& front_distance = this->sensor_distances_[1];
-
-  // The larger the free space in the front, the greater the velocity can be.
-  // The tanh function - tuned with parameters - is useful to implement such behavior smoothly.
-  this->target_velocity_ = front_distance * FACTOR * std::tanh(front_distance * FACTOR2);
+  this->target_velocity_ = 2.0; // TODO: USE SENSOR DATA TO COMPUTE THIS
 }
 
 void VehicleController::computeTargetSteeringAngle() {
-  const float FACTOR = 0.5f;
-
-  // Calcualte relative lateral position on the road
-  // (how far towards right border is the cart located)
-  const float& right_distance = this->sensor_distances_[0];
-  const float& front_distance = this->sensor_distances_[1];
-  const float& left_distance = this->sensor_distances_[2];
-  float rightShift = left_distance - right_distance;
-
-  // The closer the cart is to the right border, the more it should steer to the left
-  // (and vice versa).
-  // (Steering to the left = positive steering angle)
-  // To achieve this in a smooth way, a parameter-tuned tanh function can be used.
-  this->target_steering_angle_ = (FACTOR/front_distance) * rightShift * std::tanh( std::abs(rightShift));
+  this->target_steering_angle_ = 0.0; // TODO: USE SENSOR DATA TO COMPUTE THIS
 }
 
 void VehicleController::overwriteLidarDistances(const float distances[3]) {
